b222fc13632f2f970c5f5a6dcbdf365e766246d6
Kunal Jaykam <50500283+kunaljaykam@users.noreply.github.com>
2024-10-04 00:24:36 +0530

    Update build.js

--- build.js
+++ build.js
@@ -1,21 +1,29 @@
 const fs = require('fs');
 const path = require('path');
-require('dotenv').config();
 
+// Path to the index.html file
 const htmlFilePath = path.join(__dirname, 'index.html');
+
+// Read the content of index.html
 let htmlContent = fs.readFileSync(htmlFilePath, 'utf8');
 
+// Google Analytics script to insert
 const googleAnalyticsScript = `
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-P9J049EHG0"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-  gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}');
+  gtag('config', 'G-P9J049EHG0');
 </script>
 `;
 
-htmlContent = htmlContent.replace('</head>', `${googleAnalyticsScript}</head>`);
-fs.writeFileSync(htmlFilePath, htmlContent, 'utf8');
-// console.log('Google Analytics script added to index.html');
+// Insert the Google Analytics script before the closing </head> tag
+if (!htmlContent.includes(googleAnalyticsScript)) {
+    htmlContent = htmlContent.replace('</head>', `${googleAnalyticsScript}</head>`);
+    fs.writeFileSync(htmlFilePath, htmlContent, 'utf8');
+    console.log('Google Analytics script added to index.html');
+} else {
+    console.log('Google Analytics script is already present in index.html');
+}
